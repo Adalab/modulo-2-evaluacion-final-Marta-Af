a67ffde690a315aa472ae0587330b439ba670254
Marta <marta.af.lab@gmail.com>
2024-01-06 22:45:41 +0100

    add favourite and print favourite

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,10 @@
 // QUERYSELECTOR
 const btnSearch = document.querySelector(".js_btn");
 const listCard = document.querySelector(".js_listCard");
+const sectionFavourite = document.querySelector('.js_sectionFavourite');
+const chromesFavourites = document.querySelector('.js_chromesFavourites');
+const liFavourites = document.querySelector('.js_liFavourites');
+
 
 
 // VARIABLES DE DATOS
@@ -12,35 +16,20 @@ let chartersList = [];
 
 // FUNCIONES HANDLE
 
-//Funcion pintar uno
-function renderOne(charters) {
-  listCard.innerHTML += `
-        <li class="chrome js_oneChrome">
-            <img class="photo" src="${charters.imageUrl}" alt="" />
-            <h3>${charters.name}</h3>
-        </li>
-   `;
-}
-
-//Funcion pintar todos los cromos
-function renderAll() {
-  for (let i = 0; i < chartersList.length; i++) {
-    renderOne(chartersList[i]);
-  }
-  const oneChrome = document.querySelectorAll(".js_oneChrome");
-
-  for (const chrome of oneChrome) {
-    chrome.addEventListener("click", handleClickChromes);
-  }
-}
-
+// Evento de clic en un cromo
 function handleClickChromes(event) {
-    console.log(event.currentTarget);
-
     const favChrome = event.currentTarget;
-    favChrome.classList.toggle('favourite')
+    favChrome.classList.toggle('favourite');
 
-  }
+    // Obtener el id del cromo seleccionado
+    const selectedId = favChrome.dataset.id;
+
+    // Buscar el cromo en la lista de datos
+    const selectFav = chartersList.find(charters => charters._id == selectedId);
+
+    // Hacer algo con el cromo seleccionado (en este caso, imprimir en la consola)
+    console.log(selectFav);
+}
 
   
 // EVENTOS
@@ -50,6 +39,26 @@ function handleClickChromes(event) {
 // });
 
 // CÓDIGO CUANDO INICIA LA PÁGINA
+//Funcion pintar uno
+function renderOne(charters) {
+    listCard.innerHTML += `
+        <li class="chrome js_oneChrome" data-id="${charters._id}">
+            <img class="photo" src="${charters.imageUrl}" alt="" />
+            <h3>${charters.name}</h3>
+        </li>
+    `;
+}
+  //Funcion pintar todos los cromos
+function renderAll() {
+    for (let i = 0; i < chartersList.length; i++) {
+      renderOne(chartersList[i]);
+    }
+    const oneChrome = document.querySelectorAll(".js_oneChrome");
+  
+    for (const chrome of oneChrome) {
+      chrome.addEventListener("click", handleClickChromes);
+    }
+  }
 // Pintar los cromos
 fetch("//api.disneyapi.dev/character")
   .then((response) => response.json())
