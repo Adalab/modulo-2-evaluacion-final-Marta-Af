1e06cf2fce7fec783ee990f4d9558937359a225d
Marta <marta.af.lab@gmail.com>
2024-01-07 11:04:29 +0100

    pint chrome favourite

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,13 @@
 // QUERYSELECTOR
 const btnSearch = document.querySelector(".js_btn");
 const listCard = document.querySelector(".js_listCard");
-const sectionFavourite = document.querySelector('.js_sectionFavourite');
-const chromesFavourites = document.querySelector('.js_chromesFavourites');
-const liFavourites = document.querySelector('.js_liFavourites');
-
-
+const sectionFavourite = document.querySelector(".js_sectionFavourite");
+const chromesFavourites = document.querySelector(".js_chromesFavourites");
+const liFavourites = document.querySelector(".js_liFavourites");
 
 // VARIABLES DE DATOS
 let chartersList = [];
-
+let favourite
 
 // FUNCIONES
 
@@ -18,20 +16,25 @@ let chartersList = [];
 
 // Evento de clic en un cromo
 function handleClickChromes(event) {
-    const favChrome = event.currentTarget;
-    favChrome.classList.toggle('favourite');
+  const favChrome = event.currentTarget;
+  favChrome.classList.toggle("favourite");
 
-    // Obtener el id del cromo seleccionado
-    const selectedId = favChrome.dataset.id;
+  // Obtener el id del cromo seleccionado
+  const selectedId = favChrome.dataset.id;
 
-    // Buscar el cromo en la lista de datos
-    const selectFav = chartersList.find(charters => charters._id == selectedId);
+  // Buscar el cromo en la lista de datos
+  const selectFav = chartersList.find((charters) => charters._id == selectedId);
+  //Pintar cromo seleccionado en favoritos
+  chromesFavourites.innerHTML += `<li class="chrome js_oneChrome" data-id="${selectFav._id}">
+    <img class="photo" src="${selectFav.imageUrl}" alt="" />
+    <h3>${selectFav.name}</h3>
+</li>
+`;
 
-    // Hacer algo con el cromo seleccionado (en este caso, imprimir en la consola)
-    console.log(selectFav);
+  // Hacer algo con el cromo seleccionado (en este caso, imprimir en la consola)
+  console.log(selectFav);
 }
 
-  
 // EVENTOS
 // btnSearch.addEventListener('click', (event) => {
 //     event.preventDefault();
@@ -41,24 +44,24 @@ function handleClickChromes(event) {
 // CÓDIGO CUANDO INICIA LA PÁGINA
 //Funcion pintar uno
 function renderOne(charters) {
-    listCard.innerHTML += `
+  listCard.innerHTML += `
         <li class="chrome js_oneChrome" data-id="${charters._id}">
             <img class="photo" src="${charters.imageUrl}" alt="" />
             <h3>${charters.name}</h3>
         </li>
     `;
 }
-  //Funcion pintar todos los cromos
+//Funcion pintar todos los cromos
 function renderAll() {
-    for (let i = 0; i < chartersList.length; i++) {
-      renderOne(chartersList[i]);
-    }
-    const oneChrome = document.querySelectorAll(".js_oneChrome");
-  
-    for (const chrome of oneChrome) {
-      chrome.addEventListener("click", handleClickChromes);
-    }
+  for (let i = 0; i < chartersList.length; i++) {
+    renderOne(chartersList[i]);
   }
+  const oneChrome = document.querySelectorAll(".js_oneChrome");
+
+  for (const chrome of oneChrome) {
+    chrome.addEventListener("click", handleClickChromes);
+  }
+}
 // Pintar los cromos
 fetch("//api.disneyapi.dev/character")
   .then((response) => response.json())
@@ -68,7 +71,5 @@ fetch("//api.disneyapi.dev/character")
 
     // console.log(data.data);
   });
-
-
 
 console.log(">> Ready :)");
