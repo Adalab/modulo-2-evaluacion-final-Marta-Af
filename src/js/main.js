9a8c71eec2014dd36ae80da7859e1fa11849c993
Marta <marta.af.lab@gmail.com>
2024-01-08 19:01:38 +0100

    changes for search

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 "use strict";
 // QUERYSELECTOR
 const btnSearch = document.querySelector(".js_btn");
-const form = document.querySelector('.js_form');
+const form = document.querySelector(".js_form");
 const listCard = document.querySelector(".js_listCard");
 const sectionFavourite = document.querySelector(".js_sectionFavourite");
 const chromesFavourites = document.querySelector(".js_chromesFavourites");
 const liFavourites = document.querySelector(".js_liFavourites");
-
 
 // VARIABLES DE DATOS
 let chartersList = []; //array inicial
@@ -18,45 +17,51 @@ let favouriteData = []; //array de favoritos
 
 // Evento de clic en un cromo
 function handleClickChromes(event) {
-    const favChrome = event.currentTarget;
-  
-    const selectedId = favChrome.dataset.id; // Obtener el id del cromo seleccionado
-  console.log(selectedId);
-    const selectFav = chartersList.find((charters) => charters._id === parseInt(selectedId)); // Buscar el cromo en la lista de datos
-  console.log(selectFav);
-    const favouriteIndex = favouriteData.findIndex((charters) => charters._id === selectedId);
-  
-    if (favouriteIndex === -1) {
-      favouriteData.push(selectFav);
+  const favChrome = event.currentTarget;
 
-    } else {
-      favouriteData.splice(favouriteIndex, 1);
-    }
-  
-    renderFavourites();
-    favChrome.classList.toggle("favourite");
-    // Hacer algo con el cromo seleccionado (en este caso, imprimir en la consola)
-    console.log(selectFav); // cromo seleccionado como favorito
-    console.log(favouriteIndex);
+  const selectedId = favChrome.dataset.id; // Obtener el id del cromo seleccionado
+
+  const selectFav = chartersList.find(
+    (charters) => charters._id === parseInt(selectedId)
+  ); // Buscar el cromo en la lista de datos
+
+  const favouriteIndex = favouriteData.findIndex(
+    (charters) => charters._id === selectedId
+  );
+
+  if (favouriteIndex === -1) {
+    favouriteData.push(selectFav);
+  } else {
+    favouriteData.splice(favouriteIndex, 1);
   }
+  localStorage.setItem("favouriteData", JSON.stringify(favouriteData));
+  // pintar favoritos
+  renderFavourites();
+  favChrome.classList.toggle("favourite");
+
+  // Opcionalmente se ha eliminado la clase hidden para que aparezca la seccion de favoritos al hacer click en un cromo
+  sectionFavourite.classList.remove("hidden");
+  console.log(selectFav); // cromo seleccionado como favorito
+  console.log(favouriteIndex);
+}
 
 // EVENTOS
 // Evento click en buscar
-btnSearch.addEventListener('click', (event) => {
-    event.preventDefault();
-    // traer el value del form
-    const formValue = form.querySelector('input').value;
-    fetch(`//api.disneyapi.dev/character?name=${formValue}`)
+btnSearch.addEventListener("click", (event) => {
+  event.preventDefault();
+  // traer el value del form
+  const formValue = form.querySelector("input").value;
+  fetch(`//api.disneyapi.dev/character?name=${formValue}`)
     .then((response) => response.json())
-    .then(data => {
-        chartersList = data.data;
-        listCard.innerHTML = "";
-        renderAll(); // Renderizar después de obtener los datos de la API
-        console.log(chartersList);
+    .then((data) => {
+      chartersList = data.data;
+      listCard.innerHTML = "";
+      renderAll(); // Renderizar después de obtener los datos de la API
+      console.log(chartersList);
     });
-
-    // al click en el boton, ir a la api
 });
+
+
 
 // CÓDIGO CUANDO INICIA LA PÁGINA
 //Funcion pintar un cromo
@@ -81,23 +86,35 @@ function renderAll() {
 }
 //Pintar un favorito
 function renderOneFavourite(favouriteData) {
-    liFavourites.innerHTML += `<li class="chrome js_oneChrome">
+  liFavourites.innerHTML += `<li class="chrome js_oneChrome">
+
     <img class="photo" src="${favouriteData.imageUrl}" alt="" />
+    <button class="btnStar js_btnReset">&times;</button>
     <h3>${favouriteData.name}</h3>
 </li>`;
+
+const btnReset = document.querySelector('.js_btnReset');
+console.log(btnReset);
+btnReset.addEventListener('click', (event) => {
+    // const liToReset = event.currentTarget
+    // liToReset.remove();
+    // console.log(liToReset);
+    // console.log(event.currentTarget);
+    for(let i = 0; i < favouriteData.length; i++) 
+});
 }
 
 //Funcion pintar favoritos
 function renderFavourites() {
-    liFavourites.innerHTML ='';
-    for (let i = 0; i < favouriteData.length; i++) {
-        renderOneFavourite(favouriteData[i])
-      }
+  liFavourites.innerHTML = "";
+
+  for (let i = 0; i < favouriteData.length; i++) {
+    renderOneFavourite(favouriteData[i]);
+  }
 }
 
-
 // Pintar los cromos
-fetch('//api.disneyapi.dev/character')
+fetch("//api.disneyapi.dev/character")
   .then((response) => response.json())
   .then((data) => {
     chartersList = data.data;
